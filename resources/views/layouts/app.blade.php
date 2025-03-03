238b5d9318f84c74b25eb264d84d44337dacb707
Fiqa Halim <syafiqahalim.work@gmail.com>
2025-03-03 16:26:20 +0800

    03032025 Add Whatsapp button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,10 @@
 
     @include('partials.footer')
 
+    <a href="https://wa.me/11234567890" target="_blank" id="whatsapp-button">
+        <i class="fab fa-whatsapp"></i>
+    </a>
+
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
