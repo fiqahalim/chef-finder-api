261ae1a485212695bb09efb3a680ce53dfd821b1
Fiqa Halim <syafiqahalim.work@gmail.com>
2025-03-04 15:01:08 +0800

    04032025 Change wording for restaurant page

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
             <h5 class="section-title ff-secondary text-center text-primary fw-normal" style="margin-top:50px;margin-bottom:50px;">
-                {{ __('default.menu.chefs') }}
+                {{ __('default.menu.restaurants') }}
             </h5>
             <h1 class="mb-5">{{ __('default.global.all') }} {{ __('default.menu.restaurants') }}</h1>
         </div>
